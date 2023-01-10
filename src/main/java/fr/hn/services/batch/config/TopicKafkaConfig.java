78923dc5aa17b080315689907f80b5d518f77335
Diaguily <diaguilybouna@gmail.com>
2023-01-10 11:34:26 +0100

    first commit

--- src/main/java/fr/hn/services/batch/config/TopicKafkaConfig.java
+++ src/main/java/fr/hn/services/batch/config/TopicKafkaConfig.java
@@ -1,14 +1,24 @@
 package fr.hn.services.batch.config;
 
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
 public class TopicKafkaConfig {
-/*    @Bean
-    public NewTopic topic() {
+
+/*   @Bean
+    public NewTopic requestsForWokers() {
         return TopicBuilder.name("requestsForWokers")
                 .partitions(2)
                 .build();
-    }*/
+    }
 
+   @Bean
+    public NewTopic repliesFromWokers() {
+        return TopicBuilder.name("repliesFromWokers")
+                .partitions(2)
+                .build();
+    }*/
 }
